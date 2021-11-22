a313aac1b0af2ba1941fd5f1b40083da94d43aa5
Riswan Agam <riswanagam@gmail.com>
2021-11-22 21:31:46 +0700

    add-package

---
+++ SportReservation/app/src/main/java/com/example/sportreservation/ui/detailarticle/DetailArticleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportreservation.ui.detailarticle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportreservation.R
+
+class DetailArticleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_article)
+    }
+}
---
+++ SportReservation/app/src/main/java/com/example/sportreservation/ui/detailplace/DetailPlaceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportreservation.ui.detailplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportreservation.R
+
+class DetailPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_place)
+    }
+}
