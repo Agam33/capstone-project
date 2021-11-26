6c0014cf941413a44a786040b7c1cc4782657d45
Riswan Agam <riswanagam@gmail.com>
2021-11-26 17:45:42 +0700

    create-load-json-function

--- SportReservation/app/src/main/java/com/example/sportreservation/utils/JsonHelper.kt
+++ SportReservation/app/src/main/java/com/example/sportreservation/utils/JsonHelper.kt
@@ -1,6 +1,8 @@
 package com.example.sportreservation.utils
 
 import android.content.Context
+import com.example.sportreservation.data.source.remote.response.SportPlaceResponse
+import org.json.JSONObject
 import java.io.IOException
 
 class JsonHelper(private val context: Context) {
@@ -19,5 +21,87 @@ class JsonHelper(private val context: Context) {
         }
     }
 
+    fun loadBasketPlace(): List<SportPlaceResponse> {
+        val list = ArrayList<SportPlaceResponse>()
+        try {
+            val responseObject = JSONObject(parsingFileToString("Basket.json").toString())
+            val listArray = responseObject.getJSONArray("basket")
+            for(item in 0 until listArray.length()) {
+                val place = listArray.getJSONObject(item)
 
+                list.add(
+                    SportPlaceResponse(
+                        place.getInt("id"),
+                        place.getString("nama"),
+                        place.getString("alamat"),
+                        place.getString("buka"),
+                        place.getString("tutup"),
+                        place.getString("harga"),
+                        place.getString("noTlp"),
+                        place.getString("fasilitas"),
+                        place.getString("imgUrl")
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return list
+    }
+
+    fun loadBadmintonPlace(): List<SportPlaceResponse> {
+        val list = ArrayList<SportPlaceResponse>()
+        try {
+            val responseObject = JSONObject(parsingFileToString("Badminton.json").toString())
+            val listArray = responseObject.getJSONArray("badminton")
+            for(item in 0 until listArray.length()) {
+                val place = listArray.getJSONObject(item)
+
+                list.add(
+                    SportPlaceResponse(
+                        place.getInt("id"),
+                        place.getString("nama"),
+                        place.getString("alamat"),
+                        place.getString("buka"),
+                        place.getString("tutup"),
+                        place.getString("harga"),
+                        place.getString("noTlp"),
+                        place.getString("fasilitas"),
+                        place.getString("imgUrl")
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return list
+    }
+
+    fun loadFutsalPlace(): List<SportPlaceResponse> {
+        val list = ArrayList<SportPlaceResponse>()
+        try {
+            val responseObject = JSONObject(parsingFileToString("Futsal.json").toString())
+            val listArray = responseObject.getJSONArray("futsal")
+            for(item in 0 until listArray.length()) {
+                val place = listArray.getJSONObject(item)
+
+                list.add(
+                    SportPlaceResponse(
+                        place.getInt("id"),
+                        place.getString("nama"),
+                        place.getString("alamat"),
+                        place.getString("buka"),
+                        place.getString("tutup"),
+                        place.getString("harga"),
+                        place.getString("noTlp"),
+                        place.getString("fasilitas"),
+                        place.getString("imgUrl")
+                    )
+                )
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return list
+    }
 }
