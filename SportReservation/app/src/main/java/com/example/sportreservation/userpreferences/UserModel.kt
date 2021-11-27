4642375baa2e3cd96039c60f13322664ba7613a4
Riswan Agam <riswanagam@gmail.com>
2021-11-27 15:12:02 +0700

    add-user-preferences

---
+++ SportReservation/app/src/main/java/com/example/sportreservation/userpreferences/UserModel.kt
@@ -0,0 +1,9 @@
+package com.example.sportreservation.userpreferences
+
+data class UserModel(
+    val name: String? = null,
+    val email: String? = null,
+    val address: String? = null,
+    val phone: String? = null,
+    val imgUrl: String? = null,
+)
---
+++ SportReservation/app/src/main/java/com/example/sportreservation/userpreferences/UserPreference.kt
@@ -0,0 +1,36 @@
+package com.example.sportreservation.userpreferences
+
+import android.content.Context
+
+class UserPreference(
+    context: Context
+) {
+    private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+    fun setUser(userModel: UserModel) =
+        preferences.edit().apply {
+            putString(NAME, userModel.name)
+            putString(EMAIL, userModel.email)
+            putString(ADDRESS, userModel.address)
+            putString(PHONE, userModel.phone)
+            putString(IMAGE, userModel.imgUrl)
+        }.apply()
+
+    fun getUser(): UserModel =
+        UserModel(
+            preferences.getString(NAME, ""),
+            preferences.getString(EMAIL, ""),
+            preferences.getString(ADDRESS, ""),
+            preferences.getString(PHONE, ""),
+            preferences.getString(IMAGE, ""),
+        )
+    
+    companion object {
+        private const val PREFS_NAME = "user_pref"
+        private const val NAME = "name"
+        private const val EMAIL = "email"
+        private const val ADDRESS = "address"
+        private const val PHONE = "phone"
+        private const val IMAGE = "img"
+    }
+}
