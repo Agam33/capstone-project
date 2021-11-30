4986e634d98dabe77abaa835087e1a5c280c1dff
Riswan Agam <riswanagam@gmail.com>
2021-11-30 10:45:39 +0700

    detailPlaceViewModel

--- SportReservation/app/src/main/java/com/example/sportreservation/ui/detailplace/DetailPlaceViewModel.kt
+++ SportReservation/app/src/main/java/com/example/sportreservation/ui/detailplace/DetailPlaceViewModel.kt
@@ -1,11 +1,24 @@
 package com.example.sportreservation.ui.detailplace
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.example.sportreservation.data.SportReservationRepository
+import com.example.sportreservation.data.source.local.entity.ArticleEntity
 
 class DetailPlaceViewModel(
     private val sportReservationRepository: SportReservationRepository
 ): ViewModel() {
 
+    private var _detailId = MutableLiveData<Int>()
+    private val _detailArticle = _detailId.switchMap { id ->
+        sportReservationRepository.getArticleById(id)
+    }
 
+    val detailArticle: LiveData<ArticleEntity> = _detailArticle
+
+    fun getById(id: Int) {
+        _detailId.value = id
+    }
 }
